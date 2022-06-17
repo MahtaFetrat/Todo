c46b7aee1c9dfe878996fa35e7722d59c3b1a519
MahtaFetrat <62302965+MahtaFetrat@users.noreply.github.com>
2022-06-17 13:10:04 +0430

    Update CLI.swift

--- src/CLI.swift
+++ src/CLI.swift
@@ -2,9 +2,16 @@ import Foundation
 
 
 class CLI {
+    static func run() {
+        print("Welcome to Todo app!")
+        while true {
+            CLI.menu()
+        }
+    }
+    
     static func menu() {
         print("""
-        Welcome to Todo app! What do you want to do?
+        What do you want to do?
         1. Add a new Todo
         2. View Todo list
         3. View Todo for a special date
@@ -12,17 +19,17 @@ class CLI {
         """)
         let choice = (readLine() ?? "").trimmingCharacters(in: .whitespacesAndNewlines)
         switch choice {
-                case "1":
-                    CLI.addTodo()
-                case "2":
-                    viewTodoList()
-                case "3":
-                    CLI.viewSpecialTodo()
-                case "4":
-                    exit(0)
-                default:
-                    print("Invalid input! Please choose one of the numbers above")
-                    CLI.menu()
+        case "1":
+            CLI.addTodo()
+        case "2":
+            viewTodoList()
+        case "3":
+            CLI.viewSpecialTodo()
+        case "4":
+            exit(0)
+        default:
+            print("Invalid input! Please choose one of the numbers above")
+            CLI.menu()
         }
     }
 
@@ -40,12 +47,57 @@ class CLI {
         let name = readLine() ?? "Untitled"
         let date = CLI.inputDate()
         
-        var todo = Todo(name: name, date: date)
+        var todo = Todo(name: name, dueDate: date)
         Todo.addTodo(todo: todo)
     }
 
+    private static func getSortType () -> SortType {
+        print("""
+        Sort Todos by ...
+        1. Name
+        2. Date Created
+        3. Due Date
+        """)
+        let choice = (readLine() ?? "").trimmingCharacters(in: .whitespacesAndNewlines)
+        switch choice {
+        case "1":
+            return SortType.sortByName
+        case "2":
+            return SortType.sortByDateCreated
+        case "3":
+            return SortType.sortByDueDate
+        default:
+            print("Invalid input! Please choose one of the numbers above")
+            return CLI.getSortType()
+        }
+    } 
+
+    private static func getSortOrder() -> SortOrder {
+        print("""
+        Sort order ...
+        1. Ascending
+        2. Descending
+        """)
+        let choice = (readLine() ?? "").trimmingCharacters(in: .whitespacesAndNewlines)
+        switch choice {
+        case "1":
+            return SortOrder.ascending
+        case "2":
+            return SortOrder.descending
+        default:
+            print("Invalid input! Please choose one of the numbers above")
+            return CLI.getSortOrder()
+        }
+    }
+
     static func viewTodoList() {
-        print("2 is ure choice")
+        let sortType: SortType = CLI.getSortType()
+        let sortOrder: SortOrder = CLI.getSortOrder()
+
+        let todoList: [Todo] = Todo.sortedTodoList(sortType: sortType, sortOrder: sortOrder)
+
+        print(Todo.listViewHeader)
+        print(todoList)   
     }
 
     static func viewSpecialTodo() {
